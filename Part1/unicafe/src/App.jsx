0dd17a3dc9cef097d5d79ee1108b82a2dcda4e17
Nithish699 <nithish699734@gmail.com>
2025-04-01 15:36:39 +0530

    unicafe step 1 complete

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const Header=()=>{
+  return(
+    <h1>give feedback </h1>
   )
 }
 
-export default App
+const Button=({onClick,name})=>{
+  return(
+  <button onClick={onClick}>{name}</button>
+  )
+}
+
+const Statistics=({good,neutral,bad})=>{
+  return(
+  <div> 
+  <h2>statistics</h2>
+  <span>good {good}</span><br />
+  <span>neutral {neutral}</span><br />
+  <span>bad {bad}</span>
+  </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Button onClick={()=>setGood(good+1)} name="good" />
+      <Button onClick={()=>setNeutral(neutral+1)} name="neutral" />
+      <Button onClick={()=>setBad(bad+1)} name="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
+    </div>
+  )
+}
+
+export default App;
