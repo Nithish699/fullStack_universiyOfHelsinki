cdcdded039a3e3cd6db5216ee42eeae3684203b1
Nithish699 <nithish699734@gmail.com>
2025-04-01 15:56:13 +0530

    unicafe step 2 complete

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -13,12 +13,18 @@ const Button=({onClick,name})=>{
 }
 
 const Statistics=({good,neutral,bad})=>{
+  const all = good + neutral + bad
+  const average= (good*1+ neutral*0 + bad*-1)/all
+  const positivePrcnt = (100)*good/all
   return(
   <div> 
   <h2>statistics</h2>
   <span>good {good}</span><br />
   <span>neutral {neutral}</span><br />
-  <span>bad {bad}</span>
+  <span>bad {bad}</span><br />
+  <span>all {all}</span> <br />
+  <span>average {average}</span> <br />
+  <span>positive {positivePrcnt} %</span>
   </div>
   )
 }
