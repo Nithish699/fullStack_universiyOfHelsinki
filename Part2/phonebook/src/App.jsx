125a0f4c9af902508f40c133e45680fadc2b8b1d
Nithish699 <nithish699734@gmail.com>
2025-04-11 17:04:23 +0530

    Refactor App component to include filtering functionality and ensure unique IDs for persons. STEP 4 complete.

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,10 +3,15 @@ import Person from './components/Person'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '140-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 0 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
   ]) ;
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
   
   const addName = (event) =>{
     event.preventDefault();    
@@ -21,13 +26,20 @@ const App = () => {
     setNewNumber(event.target.value)
     console.log(event.target.value)
   };
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   const nameCheck = (newName, newNumber) => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       
     } else {
-      const nameObject = { name: newName, number: newNumber }; 
+      const nameObject = { name: newName, number: newNumber,id: persons.length }; 
       setPersons(persons.concat(nameObject));
       setNewName('');
       setNewNumber('');
@@ -39,6 +51,10 @@ const App = () => {
   return (
     <div>                         
       <h2>Phonebook</h2>
+      <div>
+         filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName} >
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -52,8 +68,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        { persons.map(person => (
-          <Person key={person.name} person={person} /> 
+        { filteredPersons.map(person => (
+          <Person key={person.id} person={person} /> 
           ))}
       </ul>
     </div>
