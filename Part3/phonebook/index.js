22f6bcff01d29ce18032dd47f96c9b0bfbc9deb7
Nithish699 <nithish699734@gmail.com>
2025-04-26 08:51:44 +0530

    Add functionality to delete a person by ID and update person list; include test request for deletion. step 4 complete.

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -23,6 +23,11 @@ let persons = [
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    { 
+      "id": "5",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
     }
 ]
 
@@ -32,9 +37,19 @@ app.get('/api/persons',(request,response) => {
     
 })
 
+app.get('/info',(request,response)=>{
+  const length = persons.length;
+  const date = new Date();
+  response.send(
+      `<p>Phonebook has info for ${length} people</p>
+       <p>${date}</p>`
+  );
+  console.log("persons info sent as reponse.")
+})
+
 app.get('/api/persons/:id',(request, response)=>{
   const id = request.params.id;
-  const person=persons.find(person => person.id === String(id));
+  const person=persons.find(person => person.id == id);
 
   if(person) {
     response.json(person);
@@ -44,18 +59,25 @@ app.get('/api/persons/:id',(request, response)=>{
     response.status(404).send(`<h1>404 </h1><h3>page not found error</h3>person info for <b>id:${id}</b> not found in serve`);
     console.log('info not found')
   }  
+
+})
+
+app.delete('/api/persons/:id',(request,response)=>{
+
+  const id = request.params.id;
+  const person = persons.find(person => person.id ==id);
+
+  if(!person){
+    console.log('person not found to delete');    
+    return response.status(404).json({error: " person not found to delete"})
+  }
+
+  persons = persons.filter(person=> person.id != id)
+  response.status(204).end();
+  console.log(`person wih id:${id} deleted from server memory!`)
   
 })
 
-app.get('/info',(request,response)=>{
-    const length = persons.length;
-    const date = new Date();
-    response.send(
-        `<p>Phonebook has info for ${length} people</p>
-         <p>${date}</p>`
-    );
-    console.log("persons info sent as reponse.")
-})
 
 
 const PORT = 3001;
